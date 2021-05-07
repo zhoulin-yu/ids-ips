aeb983af174748edca0fddedbf772376c7b54ae3
Zhoulin <zhoulin.yu@etu.umontpellier.fr>
2021-05-07 23:43:55 +0200

    add menu and button
    menu and button for testing fonction  like open new window or add text

--- src/main.js
+++ src/main.js
@@ -1,62 +1,89 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const electron = require('electron');
 const path = require('path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-    app.quit();
-}
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+// SET ENV
+//process.env.NODE_ENV = 'production';
+let addWindow;
+let mainWindow;
 
-const createWindow = () => {
-    // Create the browser window.
-    const mainWindow = new BrowserWindow({
-        width: 1600,
-        height: 1200,
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        //trun nodeIntegration to true
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        }
     });
 
     // and load the index.html of the app.
     mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    //mainWindow.webContents.openDevTools();
 
-    //Build menu from template
-    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-    //Insert Menu
+    // Close all windows when main window is closed
+    mainWindow.on('close', function() {
+        app.quit();
+    })
+
+    //----------------Build menu from template-------------------//
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+        //Insert menu
     Menu.setApplicationMenu(mainMenu);
-};
+    //----------------------------------------------------------// 
 
-// create menu template
-const maiMenuTemplate = [{
-    label: 'File'
-}];
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-//--------------------------funcion added----------------------------//
-
-
-window.getElementById("scanAll").addEventListener('click', () => {
-    //const outpout = document.getElementById("output");
-    //const data = outpout.innerText;
-    console.log("test of click");
 });
 
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+    app.quit();
+}
 
 
 
 
 
+//------------------------Handle create add window----------------//
+function createAddWindow() {
+    // Create the add window.
+    addWindow = new BrowserWindow({
+        width: 400,
+        height: 400,
+        title: 'Add file to scan',
+        //trun nodeIntegration to true
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        }
+    });
 
+    // and load the index.html of the app.
+    addWindow.loadFile(path.join(__dirname, 'addWindow.html'));
 
+    // Open the DevTools.
+    addWindow.webContents.openDevTools();
 
+    //Garbage collection handle
+    addWindow.on('close', function() {
+        addWindow = null;
+    })
+}
+//--------------------------------Catch item:add--------------------------//
 
+ipcMain.on('item:add', function(e, item) {
+    mainWindow.webContents.send('item:add', item);
+    addWindow.close();
+});
+//---------------------------------------------------------------//
 
-
-
-
-//-------------------------function finished------------------------//
 
 
 // Quit when all windows are closed, except on macOS. There, it's common
@@ -76,5 +103,91 @@ app.on('activate', () => {
     }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+//--------------------------Create menu template-------------------//
+const mainMenuTemplate = [{
+    label: 'File',
+    submenu: [{
+            label: 'Add Item',
+            click() {
+                createAddWindow();
+            }
+        },
+        {
+            label: 'Clear Items',
+            click() {
+                mainWindow.webContents.send('item:clear');
+            }
+        },
+        {
+            label: 'Quit',
+            accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+            click() {
+                app.quit();
+            }
+        }
+    ]
+}];
+//-----------------------------Button--------------------//
+
+ipcMain.on("btnclick", function(event, arg) {
+    //create new window
+    var newWindow = new BrowserWindow({
+        width: 450,
+        height: 300,
+        show: false,
+        webPreferences: {
+            webSecurity: false,
+            plugins: true,
+            nodeIntegration: false
+        }
+    }); // create a new window
+
+    var github = "https:/github.com"; // loading an external url. We can load our own another html file, like how we load index.html earlier
+
+    newWindow.loadURL(github);
+    newWindow.show();
+
+    // inform the render process that the assigned task finished. Show a message in html
+    // event.sender.send in ipcMain will return the reply to renderprocess
+    event.sender.send("btnclick-task-finished", "yes");
+});
+
+ipcMain.on("btnScan1Click", function(event, arg) {
+    //create new window
+    var newWindow2 = new BrowserWindow({
+        width: 450,
+        height: 300,
+        show: false,
+        webPreferences: {
+            webSecurity: false,
+            plugins: true,
+            nodeIntegration: false
+        }
+    }); // create a new window
+
+    newWindow2.show();
+});
+
+
+//If mac, add empty object to menu
+
+if (process.platform == 'darwin') {
+    mainMenuTemplate.unshift({ label: '' });
+}
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'Developper Tools',
+        submenu: [{
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focuseWindow) {
+                    focuseWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    })
+}
+//-----------------------------------------------------------------//
